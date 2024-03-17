c471922c143caf943e1f17300279783a7e8e8e55
antoniehuang <antonieyuchi@gmail.com>
2024-03-17 20:36:10 +0000

    Add GET endpoint which return a Cash Card

--- src/test/java/ant/cashcardapi/CashCardApiApplicationTests.java
+++ src/test/java/ant/cashcardapi/CashCardApiApplicationTests.java
@@ -1,13 +1,26 @@
 package ant.cashcardapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApiApplicationTests {
 
+	@Autowired
+	TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	void shouldReturnACashCard() throws IOException {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
