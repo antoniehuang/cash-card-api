df845bde34ef984180229a5b953d110c09ac0a18
antoniehuang <antonieyuchi@gmail.com>
2024-03-18 20:34:36 +0000

    Add CashCardRepository and @Id to CashCard

--- src/main/java/ant/cashcardapi/CashCard.java
+++ src/main/java/ant/cashcardapi/CashCard.java
@@ -2,10 +2,14 @@ package ant.cashcardapi;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 public class CashCard {
+
+    @Id // Specify id as the id for the CashCardRepository.
     private Long id;
+
     private Double amount;
 }
