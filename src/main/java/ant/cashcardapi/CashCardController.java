6a3b96ee86f8dd6e50a0eac5ad07d9cc239ea1cf
antoniehuang <antonieyuchi@gmail.com>
2024-03-18 20:19:43 +0000

    Controller response with not found when card does not exist.

--- src/main/java/ant/cashcardapi/CashCardController.java
+++ src/main/java/ant/cashcardapi/CashCardController.java
@@ -6,13 +6,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // Tells Spring that this class is a Component of type RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
